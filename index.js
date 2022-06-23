af15b736309819ff16416d217327c709258b5b86
Sadie <sadie@Sadies-MacBook-Pro.local>
2022-06-23 12:37:01 -0700

    commit tree 79d03aa639a931a9793b25c58e7b7ba1994ad38f

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(drivers, match) {
+    return drivers.filter(driversName => driversName.toUpperCase() === match.toUpperCase());
+}
+
+function fuzzyMatch(driver, letters) {
+    return driver.filter(driverName => driverName.toLowerCase().indexOf(letters.toLowerCase()) === 0);
+}
+
+function matchName(driver, name, hometown) {
+    return driver.filter(driverName => name.toLowerCase() === hometown.toLowerCase());
+}
